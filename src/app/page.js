f2e761e9c756939369ceb8aa63cc6899b9b7b387
emilien.ridard@epfedu.fr <145775801+mimile3@users.noreply.github.com>
2025-01-09 16:37:04 +0100

    en cours de réalisation login

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,35 @@
 "use client";
-
+import { FormEvent } from "react";
 import { useRouter } from "next/navigation";
 
-import Button from "./component/Button";
-
-// import { useRouter } from "next/navigation";
-
-// export default function Home() {
-//   const router = useRouter();
-
-//   return (
-//     <button onClick={() => router.push("/dashboard")}>
-//       Click here to read more
-//     </button>
-//   );
-// }
-export default function Home() {
+export default function LoginPage() {
   const router = useRouter();
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      router.push("./dashboard/page.jsx");
+    } else {
+      // Handle errors
+    }
+  }
+
   return (
-    <button onClick={() => router.push("/dashboard")}>
-      Click here to read more
-    </button>
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
   );
 }
